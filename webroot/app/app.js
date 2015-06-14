e707b97106f1a9e9f845ff2fb333422afa80cec7
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2015-06-14 16:09:23 +0200

    Init angular seed project

---
+++ webroot/app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('Microshop', [
+  'ngRoute',
+  'Microshop.view1',
+  'Microshop.view2',
+  'Microshop.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);

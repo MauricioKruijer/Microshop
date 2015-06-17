8a541ec77e49626af9b513dbdc394d3ee383c330
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2015-06-17 11:18:49 +0200

    omg :')

--- app/layout/partials/header.php
+++ app/layout/partials/header.php
@@ -1,1 +1,8 @@
 <h1>HEAD</h1>
+<ul>
+    <li><a href="/">Products</a></li>
+    <li><a href="/cart">Cart</a></li>
+    <li><a href="/user/signup">Sign up</a></li>
+    <li><a href="/user/login">Login</a></li>
+    <li><a href="/user/logout">Logout</a></li>
+</ul>
